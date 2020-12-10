e833ccd0ae0ce5ef4e335987ac0dab4a33c1a808
Shubham <shubhamsingh1840@gmail.com>
2020-12-10 22:13:33 +0530

    updated

--- random.js
+++ random.js
@@ -1,30 +1,31 @@
-let abme=document.getElementById("aboutme");
-let wexp=document.getElementById("workexperience");
-let projects=document.getElementById("projects");
-let edu=document.getElementById("education");
-let cdet=document.getElementById("contactdetails");
+let abme = document.getElementById("aboutme");
+let wexp = document.getElementById("workexperience");
+let projects = document.getElementById("projects");
+let edu = document.getElementById("education");
+let cdet = document.getElementById("contactdetails");
+document.title = "Shubham Singh";
 
-abme.addEventListener("click",function(){
-    let scrollDiv= document.querySelector(".about-container").offsetTop-100;
-    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+abme.addEventListener("click", function () {
+    let scrollDiv = document.querySelector(".about-container").offsetTop - 100;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth' });
 });
 
-wexp.addEventListener("click",function(){
-    let scrollDiv= document.querySelector(".process-wrapper").offsetTop-200;
-    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+wexp.addEventListener("click", function () {
+    let scrollDiv = document.querySelector(".process-wrapper").offsetTop - 200;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth' });
 });
 
-projects.addEventListener("click",function(){
-    let scrollDiv= document.querySelector(".services").offsetTop;
-    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+projects.addEventListener("click", function () {
+    let scrollDiv = document.querySelector(".services").offsetTop;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth' });
 });
 
-edu.addEventListener("click",function(){
-    let scrollDiv= document.querySelector(".timeline").offsetTop-100;
-    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+edu.addEventListener("click", function () {
+    let scrollDiv = document.querySelector(".timeline").offsetTop - 100;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth' });
 });
 
-cdet.addEventListener("click",function(){
-    let scrollDiv= document.querySelector("#popo").offsetTop-100;
-    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+cdet.addEventListener("click", function () {
+    let scrollDiv = document.querySelector("#popo").offsetTop - 100;
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth' });
 });
